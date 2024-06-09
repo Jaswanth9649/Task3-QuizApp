c728ef8018b81d36c752d106b08ee7ee1496ae96
Jaswanth9649 <jaswanthmanohar@gmail.com>
2024-06-09 21:09:09 +0530

    third

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question [] = {
+            "What is 15+30 ?",
+            "Who invented Computer?",
+            "What is 13*7 ?",
+            "Who is the founder of Meta?",
+            "In the given options,Which is the Example of System Software?"
+    };
+
+    public static String choices [][] = {
+            {"37" , "52" , "45" , "42"},
+            {"Einstein","Charles Babbage","Graham Bell","None of the above"},
+            {"91","87","92","90"},
+            {"Elon Musk","Mark Zuckerberg","Ratan Tata","Bill Gates"},
+            {"Linux","MacOS","Windows","All of the above"}
+    };
+
+    public static String correctAnswers []={
+            "45",
+            "Charles Babbage",
+            "91",
+            "Mark Zuckerberg",
+            "All of the above"
+    };
+}
